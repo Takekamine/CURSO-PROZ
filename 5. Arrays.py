7a7ae55f38bfd31a3d1fd7da906682112d7fc76c
Lucas Akamine <142038078+Takekamine@users.noreply.github.com>
2023-09-19 21:23:52 -0300

    Create 5. Arrays.py

---
+++ 5. Arrays.py
@@ -0,0 +1,7 @@
+array_mercado=["Pão","Leite","Ovos","Arroz","Feijão","Açucar","Sal"]
+for i in array_mercado:
+    print(i)
+
+array_anos=[2001,1971,1965,1980,1973,1987,2000]
+for x in array_anos:
+    print(x)
