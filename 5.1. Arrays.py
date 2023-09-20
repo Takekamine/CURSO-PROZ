7c0b95f085b88ba7980b33f8ac4fdb7805117dc2
Lucas Akamine <142038078+Takekamine@users.noreply.github.com>
2023-09-19 21:30:16 -0300

    Create 5.1. Arrays.py

---
+++ 5.1. Arrays.py
@@ -0,0 +1,8 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento',
+                  'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 
+                  'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+qtd=len(lista_musicos)
+print(f"Quantidade de cantores: {qtd}")
+print(f"Índice 2: {lista_musicos[1]}")
+print(f"Índice 9: {lista_musicos[8]}")
+print(f"Índice 14: {lista_musicos[13]}")
